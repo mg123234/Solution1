52a72b2c2118c142c20b2c7a8ff88e0bee1a2777
MDXLX <MDXLX@DESKTOP-A5106OB>
2024-02-19 08:28:55 +0700

    Add project files.

---
+++ WebAPI/Controllers/CategoryController.cs
@@ -0,0 +1,181 @@
+﻿using AutoMapper;
+using DomainLayer.Custom;
+using DomainLayer.DTO;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+using ServiceLayer.Service;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using WebApplication1.Areas.Admin.ViewModels;
+
+namespace WebApplication1.Areas.Admin.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _service;
+        public CategoryController(ICategoryService service)
+        {
+            _service = service;
+        }
+        // GET: CategoryController
+        public IActionResult Index(CategoryVM vModel)
+        {
+            IActionResult result;
+            if (vModel.PageInfo == null)
+            {
+                vModel.PageInfo = new PageInfo()
+                {
+                    Controller = "Category"
+                };
+            }
+            vModel.CategoryList = _service.Index(vModel.PageInfo);
+            vModel.PageSizeSelectList = new SelectList(vModel.PageInfo.PageSizeList);
+            result = View(vModel);
+            return result;
+        }
+
+        // GET: CategoryController/Details/5
+        public IActionResult Details(int id)
+        {
+            IActionResult result;
+            CategoryDetailsVM vModel = new()
+            {
+                CategoryDetailsDTO = _service.Details(id)
+            };
+            result = View(vModel);
+            return result;
+        }
+
+        // GET: CategoryController/Create
+        public IActionResult Create()
+        {
+            IActionResult result;
+            CategoryCreateVM vModel = new()
+            {
+                CategorySelectList = new SelectList(_service.CategorySelectList(), "Id", "Name").ToList()
+            };
+            vModel.CategorySelectList.Insert(0, new SelectListItem()
+            {
+                Text = "No Parent",
+                Value = ""
+            });
+            result = View(vModel);
+            return result;
+
+        }
+
+        // POST: CategoryController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CategoryCreateVM vModel)
+        {
+            IActionResult result;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _service.Create(vModel.CategoryCreateDTO);
+                    result = RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    vModel.CategorySelectList = new SelectList(_service.CategorySelectList(), "Id", "Name").ToList();
+                    vModel.CategorySelectList.Insert(0, new SelectListItem()
+                    {
+                        Text = "No Parent",
+                        Value = "null"
+                    });
+                    result = View(vModel);
+                }
+            }
+            catch
+            {
+                result = StatusCode(500, "Internal server error");
+            }
+            return result;
+        }
+
+        // GET: CategoryController/Edit/5
+        public IActionResult Edit(int id)
+        {
+            IActionResult result;
+            CategoryEditVM vModel = new()
+            {
+                CategorySelectList = new SelectList(_service.CategorySelectList(), "Id", "Name").ToList(),
+                CategoryEditDTO = _service.Edit(id)
+            };
+            vModel.CategorySelectList.Insert(0, new SelectListItem()
+            {
+                Value = "",
+                Text = "No Parent"
+            });
+            result = View(vModel);
+            return result;
+        }
+
+        // POST: CategoryController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(CategoryEditVM vModel)
+        {
+            IActionResult result;
+            //try
+            //{
+                if (ModelState.IsValid)
+                {
+                    _service.Update(vModel.CategoryEditDTO);
+                    result = RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    vModel.CategorySelectList = new SelectList(_service.CategorySelectList(), "Id", "Name").ToList();
+                    vModel.CategorySelectList.Insert(0, new SelectListItem()
+                    {
+                        Value = null,
+                        Text = "No Parent"
+                    });
+                    result = View(vModel);
+                }
+            //}
+            //catch
+            //{
+            //    result = StatusCode(500, "Internal server error");
+            //}
+            return result;
+        }
+
+        // GET: CategoryController/Delete/5
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CategoryController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(List<int> idList)
+        {
+            IActionResult result;
+            try
+            {
+                foreach (var item in idList)
+                {
+                    _service.Delete(item);
+                }
+                result = Json(true);
+            }
+            catch
+            {
+                result = View();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ WebAPI/Controllers/PostController.cs
@@ -0,0 +1,181 @@
+﻿using DomainLayer.Custom;
+using DomainLayer.DTO;
+using DomainLayer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ServiceLayer.Service;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Areas.Admin.ViewModels;
+
+namespace WebApplication1.Areas.Admin.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PostController : Controller
+    {
+        private readonly IPostService _service;
+        private readonly UserManager<AppUser> _userManager;
+        public PostController(IPostService service, UserManager<AppUser> userManager)
+        {
+            _service = service;
+            _userManager = userManager;
+        }
+        // GET: PostController
+        [HttpGet]
+        public IActionResult Index(PostVM vModel)
+        {
+            IActionResult result;
+            if (vModel.PageInfo == null)
+            {
+                vModel.PageInfo = new PageInfo()
+                {
+                    Controller="Post"
+                };
+            }
+            result = View(vModel);
+            vModel.PostList = _service.Index(vModel.PageInfo);
+            vModel.PageSizeSelectList = new SelectList(vModel.PageInfo.PageSizeList);
+            return result;
+        }
+
+        // GET: PostController/Details/5
+        [HttpGet("{id:int}")]
+        public IActionResult Details(int id)
+        {
+            IActionResult result;
+            PostDetailsVM vModel = new()
+            {
+                PostDetailsDTO = _service.Details(id) 
+            };
+            List<SelectListItem> list = new();
+            foreach(var item in vModel.PostDetailsDTO.CategoryList)
+            {
+                list.Add(new SelectListItem()
+                {
+                    Text=item.CategorySelectDTO.Name
+                });
+            }
+            vModel.CategorySelectList = list;
+            result = View(vModel);
+            return result;
+        }
+
+        // GET: PostController/Create
+        public IActionResult Create()
+        {
+            IActionResult result;
+            PostCreateVM vModel = new ()
+            {
+                CategorySelectList = new SelectList(_service.CategorySelectList(), "Id", "Name").ToList()
+            };
+            result = View(vModel);
+            return result;
+
+        }
+
+        // POST: PostController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateAsync(PostCreateVM vModel)
+        {
+            IActionResult result;
+            //try
+            //{
+                if (ModelState.IsValid)
+                {
+                    var user = await _userManager.GetUserAsync(this.User);
+                    vModel.PostCreateDTO.UserId = user.Id;
+                    _service.Create(vModel.PostCreateDTO);
+                    result = RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    vModel.CategorySelectList = new SelectList(_service.CategorySelectList(), "Id", "Name").ToList();
+                    vModel.CategorySelectList.Insert(0, new SelectListItem()
+                    {
+                        Text = "No Parent",
+                        Value = null
+                    });
+                    result = View(vModel);
+                }
+            //}
+            //catch
+            //{
+            //    result = StatusCode(500, "Internal server error");
+            //}
+            return result;
+        }
+
+        // GET: PostController/Edit/5
+        public IActionResult Edit(int id)
+        {
+            IActionResult result;
+            PostEditVM vModel = new()
+            {
+                CategorySelectList = new MultiSelectList(_service.CategorySelectList(), "Id", "Name").ToList(),
+                PostEditDTO = _service.Edit(id)
+            };
+            vModel.PostEditDTO.CategoryIdList = vModel.PostEditDTO.PostCategories.Select(x => x.CategoryId).ToList();
+            result = View(vModel);
+            return result;
+        }
+
+        // POST: PostController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(PostEditVM vModel)
+        {
+            IActionResult result;
+            //try
+            //{
+                if (ModelState.IsValid)
+                {
+                    _service.Update(vModel.PostEditDTO);
+                    result = RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    vModel.CategorySelectList = new SelectList(_service.CategorySelectList(), "Id", "Name").ToList();
+                    result = View(vModel);
+                }
+            //}
+            //catch
+            //{
+            //    result = StatusCode(500, "Internal server error");
+            //}
+            return result;
+        }
+
+        // GET: PostController/Delete/5
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PostController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(List<int> idList)
+        {
+            IActionResult result;
+            try
+            {
+                foreach (var item in idList)
+                {
+                    _service.Delete(item);
+                }
+                result = Json(true);
+            }
+            catch
+            {
+                result = View();
+            }
+
+            return result;
+        }
+    }
+}
